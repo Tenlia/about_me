a8437374a41e4fa5641d37b57a75a68ec888da2b
Elizabeth Kleinschmidt <tenlia@hotmail.com>
2016-06-14 16:17:17 -0700

    replaced all the text with my own text

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 'use strict';
 
-var userName = prompt('Greetings! What is your name?');
+var userName = prompt('Hello! What is your name?');
 alert('Welcome ' + userName + ' glad you\'re here!');
 
-var answer1 = prompt('Does Same have 3 cats?').toLowerCase();
+var answer1 = prompt('What\'s Elizabeth\'s name?').toLowerCase();
 
-if(answer1 === 'yes' || answer1 === 'y') {
-  alert('Right! Same has felines named Buddy, Alistair and Trillian.');
+if(answer1 === 'elizabeth' || answer1 === 'liz') {
+  alert('Right! Damn, I thought I got you with that one...');
 }else{
-  alert('Wrong, Sam actually does have 3 cats. Weird dude...');
+  alert('Wrong, Ha! Tricky question, but you did well, I think...');
 }
