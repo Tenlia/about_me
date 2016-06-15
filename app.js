568a9e70b99d0201f148e9aa458dab925009eca3
Elizabeth Kleinschmidt <tenlia@hotmail.com>
2016-06-15 08:58:28 -0700

    corrected my horrible grammer, and proabably made more mistakes

--- app.js
+++ app.js
@@ -11,23 +11,23 @@ if(answer1 === 'elizabeth' || answer1 === 'liz') {
   alert('Wrong, Ha! Tricky question, but you did well, I think...');
 }
 
-var answer2 = prompt('Did Elizabeth move to Seattle, Washington because of pumas?').toLowerCase();
+var answer2 = prompt('Did The Elizabeth move to Seattle, Washington because of pumas?').toLowerCase();
 
 if(answer2 === 'no' || answer2 === 'n') {
-  alert('Correct! Elizabeth moved to Seattle to escape the sun.');
+  alert('Correct! The Elizabeth moved to Seattle to escape the evil California sun.');
 }else{
-  alert('Incorrect, Elizabeth moved to Seattle to get away from the unrelenting California sun.');
+  alert('Incorrect, The Elizabeth moved to Seattle to get away from the unrelenting California sun.');
 }
 
 var answer3 = prompt('Can you drop someone(Elizabeth) in a pool to see if they\'ll swim?');
 
 if(answer3 === 'yes' || answer3 === 'y') {
-  alert('That\'s right! In-fact, that would Elizabeth\'s primary form of education!');
+  alert('That\'s right! In-fact, that would be The Elizabeth\'s primary form of education!');
 }else{
-  alert('Wrong, of course you can drop something into a pool to see if it\'ll swim. It\'s just not recommended with small children or the elderly.');
+  alert('Wrong, of course you can drop someone into a pool to see if they\'ll swim. It\'s just not recommended with small children or the elderly.');
 }
 
-var answer4 = prompt('Would you trust Elizabeth with your computer?').toLowerCase();
+var answer4 = prompt('Would you trust The Elizabeth with your computer?').toLowerCase();
 
 if(answer4 === 'no' || answer4 === 'n') {
   alert('Right! Wait, how\'d I make that the right answer? You\'re supposed to trust me!');
@@ -35,12 +35,12 @@ if(answer4 === 'no' || answer4 === 'n') {
   alert('Wrong! Wait, no, I meant that to be right! You can trust me with a computer, really!');
 }
 
-var answer5 = prompt('Is Elizabeth safe in the sunlight?').toLowerCase();
+var answer5 = prompt('Is The Elizabeth safe in the sunlight?').toLowerCase();
 
 if(answer5 === 'no' || answer5 === 'n') {
   alert('Correct! The Elizabeth is sensitive to the light. The Sun is her natural enemy.');
 }else{
-  alert('Incorrect! Don\'t put the Elizabeth in the sun, she will sizzle like a grape in a microwave.');
+  alert('Incorrect! Don\'t put The Elizabeth in the sun, she will sizzle like a grape in a microwave.');
 }
 
 var answer6 = prompt('Is this a yes or no question?').toLowerCase();
